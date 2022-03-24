efdc25828e8c0f12b0fbc65bd5ee2a2e2ede6c99
Omidaniel <omidaniel.od@gmail.com>
2022-03-24 12:45:31 -0700

    task 0 of functions and nested loops

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+A project about nested loops, header files, variable scope, declaring and defining functions in C
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef main_h
+#define main_h
+
+/*
+ * File: main.h
+ * Description: Header file containing declarations for all functions
+ *              used in the 0x02-functions_nested_loops directory.
+ */
+
+int _putchar(char c);
+
+#endif
