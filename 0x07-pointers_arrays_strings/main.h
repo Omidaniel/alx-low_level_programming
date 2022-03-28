9133b3bda4923678229f25efe58cf2d1448579a1
Omidaniel <omidaniel.od@gmail.com>
2022-03-28 07:28:28 -0700

    more pointers

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: holberton.h
+ * Descroption: Header file containing prototypes for all functions
+ *              used in the 0x07-pointers_arrays_strings directory.
+ */
+
+#include <stddef.h>
+
+int _putchar(char c);
+void *_memset(void *s, int b, size_t n);
+
+#endif
