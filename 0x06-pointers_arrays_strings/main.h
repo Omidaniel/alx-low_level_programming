9d50df564fd5e790639b615f49c5f51f52435532
Omidaniel <omidaniel.od@gmail.com>
2022-03-29 07:23:04 -0700

    adding more functions

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Description: Header file containing prototypes for all functions
+ *              used in the 0x06-pointers_arrays_strings directory.
+ */
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+
+#endif
